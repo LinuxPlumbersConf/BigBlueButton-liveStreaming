6b94eade74ad26a47cf603e1e3733d9a4abafe42
Guy Lunardi <glunardi@gmail.com>
2020-08-19 13:42:16 -0400

    lpc2020 script to provision new streamer nodes

---
+++ generate-docker-compose-file.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# You must pass exactly 5 parameters:
+# $1 - local hostname (i.e. streamer2)
+# $2 - target bbb server hostname to stream from (i.e. bbb2)
+# $3 - target bbb server secret
+# $4 - target bbb meetingID (i.e. "Hackroom 1")
+# $5 - YouTube Streaming key to use 
+#
+
+cat > docker-compose.yml << EOF
+version: '3.3'
+services:
+  bbb-streamer:
+    image: lpc2020streamer
+    build: .
+    container_name: lpc2020streamercontainer
+    environment:
+      - BBB_URL=https://$2.2020.linuxplumbersconf.org/bigbluebutton/api
+      - BBB_SECRET=$3
+      - BBB_MEETING_ID=$4
+      - BBB_USER_NAME=YouTube Live
+      - BBB_AS_MODERATOR=true
+      - BBB_ATTENDEE_PASSWORD=LPCattendee
+      - BBB_MODERATOR_PASSWORD=LPCmoderator
+      - BBB_STREAM_URL=rtmp://a.rtmp.youtube.com/live2/$5
+      - TZ=America/New_York
+
+    volumes:
+      - ./videodata:/video
+EOF
+
